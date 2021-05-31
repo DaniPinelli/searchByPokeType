a3a387af983dd7219be394a20edbc18aa41dba3e
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-05-31 17:38:54 -0300

    Working on fix print type 2 undefined

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 window.onload = () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/1 "
 
-    const arrayUsers = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.types[2].type.name)
+    for (let i = 1; i < 12; i++) {
+        let url = "https://pokeapi.co/api/v2/pokemon/" + i;
+        const arrayPoke = fetch(url)
+            .then(response => response.json())
+            .then(data => {
 
 
-                // document.querySelector("row").innerHTML += " <div class='row'><div class='col-lg-4'><div class='card w-50'><div class='card-body'><h5 class='card-title'>Name</h5><p class='card-text'>Type</p><button class='btn btn-primary'>Add to favs</button></div></div></div></div>"
 
-                .catch(err => (console.log(err)));
+                let pokemon = {
+                    name: data.name,
+                    id: data.id,
+                    image: data.sprites.front_default,
+                    type: [data.types[0].type.name]
+                };
 
-        });
+                if (data.types[1].type.name != undefined) {
+                    pokemon.type.push(" " + data.types[1].type.name);
+                }
 
+
+
+                document.querySelector("div").innerHTML += " <div class='row'><div class='col-lg-4'><div class='card w-50'><div class='card-body\
+                '><img class='photo' src=" + pokemon.image + "><h5 class='card-title'>" + pokemon.name + "\
+                </h5><p class='card-text'>Type: " + pokemon.type + "\
+                </p><button class='btn btn-primary'>Add to favs</button></div></div></div></div>"
+
+            })
+
+
+
+
+
+            .catch(err => (console.log(err)));
+    }
 }
