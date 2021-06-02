3a8d0246bbed18fb5844d266ea5e27b872523428
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-06-01 21:20:13 -0300

    Final changes

--- main.js
+++ main.js
@@ -1,55 +1,62 @@
 window.onload = () => {
 
-    let search = () => {
+    for (let i = 1; i < 151; i++) {
+        let url = "https://pokeapi.co/api/v2/pokemon/" + i;
+        const arrayPoke = fetch(url)
+            .then(response => response.json())
+            .then(data => {
 
 
+                let pokemon = {
+                    name: data.name,
+                    id: data.id,
+                    image: data.sprites.front_default,
+                    type: data.types[0].type.name
+                };
 
-        for (let i = 1; i < 13; i++) {
-            let url = "https://pokeapi.co/api/v2/pokemon/" + i;
-            const arrayPoke = fetch(url)
-                .then(response => response.json())
-                .then(data => {
+                if (data.types.length > 1) {
+                    pokemon.type = (`${data.types[0].type.name}, ${data.types[1].type.name}`)
+                } else {
+                    pokemon.type = data.types[0].type.name
+                };
+
+                document.querySelector(".pokeSubContainer").innerHTML += "<div class='item\
+                 '><img class='photo pokeImg' src=" + pokemon.image + "><h5 class='card-title pokeItemTitle'>" + pokemon.name + "\
+                 </h5><p class='pokeP'>Type: " + pokemon.type + "\
+                 </p><button class='pokeButton'>Add to favs</button></div>"
+
+                let search = () => {
 
                     let searchField = document.getElementById('input');
                     let typeSearched = searchField.value.toLowerCase().trim();
 
-                    let pokemon = {
-                        name: data.name,
-                        id: data.id,
-                        image: data.sprites.front_default,
-                        // type: data.types[0].type.name
-
-                    };
-
-                    if (data.types.length > 1) {
-                        pokemon.type = (`${data.types[0].type.name}, ${data.types[1].type.name}`)
-                    } else {
-                        pokemon.type = data.types[0].type.name
+                    if (typeSearched === pokemon.type) {
+                        document.getElementById("pokeSubContainer").style.display = 'none';
+                        document.querySelector(".pokeSubContainerSearch").innerHTML += "<div class='item\
+                         '><img class='photo pokeImg' src=" + pokemon.image + "><h5 class='card-title pokeItemTitle'>" + pokemon.name + "\
+                         </h5><p class='pokeP'>Type: " + pokemon.type + "\
+                         </p><button class='pokeButton'>Add to favs</button></div>"
                     }
-
-
-                    if (typeSearched === data.types[0].type.name || typeSearched === data.types[1].type.name) {
-                        document.querySelector(".pokeSubContainer").innerHTML += "<div class='item\
-                '><img class='photo pokeImg' src=" + pokemon[i].image + "><h5 class='card-title pokeItemTitle'>" + pokemon[i].name + "\
-                </h5><p class='pokeP'>Type: " + pokemon[i].type + "\
-                </p><button class='pokeButton'>Add to favs</button></div>"
-
-                    } else {
-                        document.querySelector(".pokeSubContainer").innerHTML += "<div class='item\
-                    '><img class='photo pokeImg' src=" + pokemon.image + "><h5 class='card-title pokeItemTitle'>" + pokemon.name + "\
-                    </h5><p class='pokeP'>Type: " + pokemon.type + "\
-                    </p><button class='pokeButton'>Add to favs</button></div>"
-                    }
+                }
+                let searchBtn = document.getElementById('button');
+                searchBtn.addEventListener('click', search);
 
 
 
-                })
 
-                .catch(err => (console.log(err)));
-        }
+            })
+            .catch(err => (console.log(err)));
+
+
+
+
+
+
+
+
+
     }
 
-    let searchBtn = document.getElementById('button');
-    searchBtn.addEventListener('click', search);
-    search();
+
+
 }
