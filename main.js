287d6e688d65432bda9eb7aff9d35062749455a9
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-06-02 15:03:40 -0300

    Separate js functions

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 window.onload = () => {
 
-    for (let i = 1; i < 151; i++) {
+
+
+    for (let i = 1; i < 13; i++) {
         let url = "https://pokeapi.co/api/v2/pokemon/" + i;
         const arrayPoke = fetch(url)
             .then(response => response.json())
             .then(data => {
-
 
                 let pokemon = {
                     name: data.name,
@@ -24,39 +25,50 @@ window.onload = () => {
                  '><img class='photo pokeImg' src=" + pokemon.image + "><h5 class='card-title pokeItemTitle'>" + pokemon.name + "\
                  </h5><p class='pokeP'>Type: " + pokemon.type + "\
                  </p><button class='pokeButton'>Add to favs</button></div>"
+            })
+            .catch(err => (console.log(err)));
+    }
 
-                let search = () => {
+    let search = () => {
+
+        for (let i = 1; i < 13; i++) {
+            let url = "https://pokeapi.co/api/v2/pokemon/" + i;
+            const arrayPoke = fetch(url)
+                .then(response => response.json())
+                .then(data => {
+
+                    let pokemon = {
+                        name: data.name,
+                        id: data.id,
+                        image: data.sprites.front_default,
+                        type: data.types[0].type.name
+                    };
+
+                    if (data.types.length > 1) {
+                        pokemon.type = (`${data.types[0].type.name}, ${data.types[1].type.name}`)
+                    } else {
+                        pokemon.type = data.types[0].type.name
+                    };
 
                     let searchField = document.getElementById('input');
                     let typeSearched = searchField.value.toLowerCase().trim();
+                    //let pokeName = indexOf(pokemon.name)
+                    // console.log(pokeName);
 
-                    if (typeSearched === pokemon.type) {
+                    if (typeSearched == pokemon.name) {
                         document.getElementById("pokeSubContainer").style.display = 'none';
                         document.querySelector(".pokeSubContainerSearch").innerHTML += "<div class='item\
-                         '><img class='photo pokeImg' src=" + pokemon.image + "><h5 class='card-title pokeItemTitle'>" + pokemon.name + "\
-                         </h5><p class='pokeP'>Type: " + pokemon.type + "\
-                         </p><button class='pokeButton'>Add to favs</button></div>"
+                                    '><img class='photo pokeImg' src=" + pokemon.image + "><h5 class='card-title pokeItemTitle'>\
+                                    " + pokemon.name + "\
+                                     </h5><p class='pokeP'>Type: " + pokemon.type + "\
+                                     </p><button class='pokeButton'>Add to favs</button></div>"
                     }
-                }
-                let searchBtn = document.getElementById('button');
-                searchBtn.addEventListener('click', search);
 
 
+                })
 
-
-            })
-            .catch(err => (console.log(err)));
-
-
-
-
-
-
-
-
-
+        }
     }
-
-
-
+    let searchBtn = document.getElementById('button');
+    searchBtn.addEventListener('click', search);
 }
