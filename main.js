f5a2d60e4c66f3401b8a56bd1d86764dd23c6038
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-06-24 14:23:36 -0300

    Added myFavs

--- main.js
+++ main.js
@@ -1,112 +1,98 @@
 let arrayPoke = [];
 
-
 let addToFavs = (addBtn) => {
+  addBtn.style.display = "none";
+  let idPoke = addBtn.id;
+  let btnRemove = document.getElementsByClassName(`rem${idPoke}`);
+  btnRemove[0].style.display = "inline-block";
 
+  let idLessOne = idPoke - 1;
 
-    addBtn.style.display = "none";
-    let idPoke = addBtn.id;
-    let btnRemove = document.getElementsByClassName(`rem${idPoke}`);
-    btnRemove[0].style.display = "inline-block";
+  if (JSON.parse(localStorage.getItem("myFavs")) !== null) {
+    myFavs = JSON.parse(localStorage.getItem("myFavs"));
+  } else {
+    myFavs = [];
+  }
 
-    let idLessOne = (idPoke - 1);
-
-    if (JSON.parse(localStorage.getItem("myFavs")) !== null) {
-        myFavs = JSON.parse(localStorage.getItem("myFavs"));
-    } else {
-        myFavs = [];
-    }
-
-    let pokemon = arrayPoke[idLessOne];
-    myFavs.push(pokemon);
-    let myFavsJSON = JSON.stringify(myFavs);
-    localStorage.setItem("myFavs", myFavsJSON);
-
-}
+  let pokemon = arrayPoke[idLessOne];
+  myFavs.push(pokemon);
+  let myFavsJSON = JSON.stringify(myFavs);
+  localStorage.setItem("myFavs", myFavsJSON);
+};
 
 let removeToFavs = (remBtn) => {
+  remBtn.style.display = "none";
+  let idPoke = remBtn.id;
+  let btnAdd = document.getElementsByClassName(`add${idPoke}`);
+  btnAdd[0].style.display = "inline-block";
 
-    remBtn.style.display = "none";
-    let idPoke = remBtn.id;
-    let btnAdd = document.getElementsByClassName(`add${idPoke}`);
-    btnAdd[0].style.display = "inline-block";
-
-
-    let myFavsArray = JSON.parse(localStorage.getItem("myFavs"));
-    let pokeIndex = myFavsArray.findIndex(pokemon => pokemon.id == idPoke);
-    myFavsArray.splice(pokeIndex, 1);
-    let pokeArrayJSON = JSON.stringify(myFavsArray);
-    localStorage.setItem("myFavs", pokeArrayJSON);
-}
-
+  let myFavsArray = JSON.parse(localStorage.getItem("myFavs"));
+  let pokeIndex = myFavsArray.findIndex((pokemon) => pokemon.id == idPoke);
+  myFavsArray.splice(pokeIndex, 1);
+  let pokeArrayJSON = JSON.stringify(myFavsArray);
+  localStorage.setItem("myFavs", pokeArrayJSON);
+};
 
 let showPoke = async () => {
-    for (let i = 1; i < 13; i++) {
+  for (let i = 1; i < 13; i++) {
+    let pokesData = await fetch("https://pokeapi.co/api/v2/pokemon/" + i);
+    const poke = await pokesData.json();
 
-        let pokesData = await fetch("https://pokeapi.co/api/v2/pokemon/" + i)
-        const poke = await pokesData.json();
+    let pokemon = {
+      name: poke.name,
+      id: poke.id,
+      image: poke.sprites.front_default,
+      type: poke.types[0].type.name,
+    };
 
-        let pokemon = {
-            name: poke.name,
-            id: poke.id,
-            image: poke.sprites.front_default,
-            type: poke.types[0].type.name
-        };
-
-        if (poke.types.length > 1) {
-            pokemon.type = (`${poke.types[0].type.name}, ${poke.types[1].type.name}`)
-        } else {
-            pokemon.type = poke.types[0].type.name
-        };
-
-        arrayPoke.push(pokemon);
+    if (poke.types.length > 1) {
+      pokemon.type = `${poke.types[0].type.name}, ${poke.types[1].type.name}`;
+    } else {
+      pokemon.type = poke.types[0].type.name;
     }
 
-    let print = () => {
-        for (let i = 0; i < arrayPoke.length; i++) {
-            let print = document.querySelector(".pokeSubContainer");
-            print.innerHTML += `<div class='item' id="div${arrayPoke[i].id}"\
+    arrayPoke.push(pokemon);
+  }
+
+  let print = () => {
+    for (let i = 0; i < arrayPoke.length; i++) {
+      let print = document.querySelector(".pokeSubContainer");
+      print.innerHTML += `<div class='item' id="div${arrayPoke[i].id}"\
                '><img class='photo pokeImg' src="${arrayPoke[i].image}"><h5 class='card-title pokeItemTitle'>"${arrayPoke[i].name}"\
                </h5><p class='pokeP'>Type: "${arrayPoke[i].type}"\
                </p><button class='pokeButton add${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='addToFavs(this)'>Add to favs</button><button\
-                class='remove rem${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='removeToFavs(this)'>Remove</button></div>`
-
-        }
+                class='remove rem${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='removeToFavs(this)'>Remove</button></div>`;
     }
+  };
 
-    print();
+  print();
 
-    let search = () => {
-
-
-        for (let i = 0; i < arrayPoke.length; i++) {
-            let card = document.getElementById("div" + (i + 1));
-            card.style.display = 'none';
-            let searchField = document.getElementById('input');
-            let typeSearched = searchField.value.toLowerCase().trim();
-            if (arrayPoke[i].name.includes(typeSearched)) {
-                card.style.display = 'block';
-            }
-        }
-        searchBtn.removeEventListener('click', search);
+  let search = () => {
+    for (let i = 0; i < arrayPoke.length; i++) {
+      let card = document.getElementById("div" + (i + 1));
+      card.style.display = "none";
+      let searchField = document.getElementById("input");
+      let typeSearched = searchField.value.toLowerCase().trim();
+      if (arrayPoke[i].name.includes(typeSearched)) {
+        card.style.display = "block";
+      }
     }
+    searchBtn.removeEventListener("click", search);
+  };
 
-    let searchBtn = document.getElementById('button');
-    searchBtn.addEventListener('click', search);
+  let searchBtn = document.getElementById("button");
+  searchBtn.addEventListener("click", search);
 
-    window.addEventListener('keyup', () => {
-        if (document.getElementById('input').value == "") {
-            searchBtn = document.getElementById('button');
-            searchBtn.addEventListener('click', search);
-            for (let i = 0; i < arrayPoke.length; i++) {
-                let card = document.getElementById("div" + (i + 1));
-                card.style.display = 'block';
-            }
-        }
-
-    });
-
-}
-
+  window.addEventListener("keyup", () => {
+    if (document.getElementById("input").value == "") {
+      searchBtn = document.getElementById("button");
+      searchBtn.addEventListener("click", search);
+      for (let i = 0; i < arrayPoke.length; i++) {
+        let card = document.getElementById("div" + (i + 1));
+        card.style.display = "block";
+      }
+    }
+  });
+};
 
 showPoke();
