569a8f0363c60598f449efe553cd206035ec963f
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-06-25 12:46:58 -0300

    Working on localStorage.js

--- localStorage.js
+++ localStorage.js
@@ -1,13 +1,13 @@
 let arrayLocal = JSON.parse(localStorage.getItem("myFavs"));
 
-//console.log(arrayLocal);
+let printFavs = () => {
+  for (let i = 0; i < arrayLocal.length; i++) {
+    let localFavs = document.querySelector(".localContainer");
+    localFavs.innerHTML += `<div class='item' id="div${arrayLocal[i].id}"><img class='photo pokeImg' src="${arrayLocal[i].image}"><h5 class='card-title pokeItemTitle'>"${arrayLocal[i].name}"\
+                 </h5><p class='pokeP'>Type: "${arrayLocal[i].type}"\
+                 </p><button\
+                  class='removeFavs rem${arrayLocal[i].id}' id='${arrayLocal[i].id}' onclick='removeToFavs(this)'>Remove</button></div>`;
+  }
+};
 
-for (let i = 0; i < arrayLocal.length; i++) {
-  document.querySelector(".localContainer");
-  print.innerHTML += `<div><img class='photo pokeImg' src="${arrayLocal[i].image}"><h5 class='card-title pokeItemTitle'>"${arrayLocal[i].name}"\
-               </h5><p class='pokeP'>Type: "${arrayLocal[i].type}"\
-               </p><button class='pokeButton add${arrayLocal[i].id}' id='${arrayLocal[i].id}' onclick='addToFavs(this)'>Add to favs</button><button\
-                class='remove rem${arrayLocal[i].id}' id='${arrayLocal[i].id}' onclick='removeToFavs(this)'>Remove</button></div>`;
-
-  console.log("ok");
-}
+printFavs();
