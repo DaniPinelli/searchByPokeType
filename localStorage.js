898dcfa4f27a300745d4b7dac39316a32301ad50
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-06-27 14:36:00 -0300

    Revome from myFavs works

--- localStorage.js
+++ localStorage.js
@@ -1,8 +1,8 @@
 let arrayLocal = JSON.parse(localStorage.getItem("myFavs"));
+let localFavs = document.querySelector(".localContainer");
 
 let printFavs = () => {
   for (let i = 0; i < arrayLocal.length; i++) {
-    let localFavs = document.querySelector(".localContainer");
     localFavs.innerHTML += `<div class='item' id="div${arrayLocal[i].id}"><img class='photo pokeImg' src="${arrayLocal[i].image}"><h5 class='card-title pokeItemTitle'>"${arrayLocal[i].name}"\
                  </h5><p class='pokeP'>Type: "${arrayLocal[i].type}"\
                  </p><button\
@@ -10,16 +10,26 @@ let printFavs = () => {
   }
 };
 
-let removeToFavs = (remBtn) => {
-  console.log(remBtn);
-  let idPoke = remBtn.id;
+let printLocal = () => {
+  arrayLocal = JSON.parse(localStorage.getItem("myFavs"));
+  for (let i = 0; i < arrayLocal.length; i++) {
+    localFavs = document.querySelector(".localContainer");
+    localFavs.innerHTML += `<div><div class='item' id="div${arrayLocal[i].id}"><img class='photo pokeImg' src="${arrayLocal[i].image}"><h5 class='card-title pokeItemTitle'>"${arrayLocal[i].name}"\
+                 </h5><p class='pokeP'>Type: "${arrayLocal[i].type}"\
+                 </p><button\
+                  class='removeFavs rem${arrayLocal[i].id}' id='${arrayLocal[i].id}' onclick='removeToFavs(this)'>Remove</button></div>`;
+  }
+};
 
+let removeToFavs = (remBtn) => {
+  let idPoke = remBtn.id;
   let myFavsArray = JSON.parse(localStorage.getItem("myFavs"));
   let pokeIndex = myFavsArray.findIndex((pokemon) => pokemon.id == idPoke);
   myFavsArray.splice(pokeIndex, 1);
   let pokeArrayJSON = JSON.stringify(myFavsArray);
   localStorage.setItem("myFavs", pokeArrayJSON);
-  window.location.reload();
+  localFavs.innerHTML = "";
+  printLocal();
 };
 
 printFavs();
