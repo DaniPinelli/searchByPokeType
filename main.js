a2f913c76a173b869c6b977fb95b884e717839fa
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-06-27 20:48:33 -0300

    Chages on main js

--- main.js
+++ main.js
@@ -34,38 +34,41 @@ let removeToFavs = (remBtn) => {
 };
 
 let showPoke = async () => {
-  for (let i = 1; i < 151; i++) {
-    let pokesData = await fetch("https://pokeapi.co/api/v2/pokemon/" + i);
-    const poke = await pokesData.json();
+  let initialArray = JSON.parse(localStorage.getItem("myFavs"));
+  console.log(initialArray.length);
+  if (initialArray.length === 0) {
+    for (let i = 1; i < 13; i++) {
+      let pokesData = await fetch("https://pokeapi.co/api/v2/pokemon/" + i);
+      const poke = await pokesData.json();
 
-    let pokemon = {
-      name: poke.name,
-      id: poke.id,
-      image: poke.sprites.front_default,
-      type: poke.types[0].type.name,
+      let pokemon = {
+        name: poke.name,
+        id: poke.id,
+        image: poke.sprites.front_default,
+        type: poke.types[0].type.name,
+      };
+
+      if (poke.types.length > 1) {
+        pokemon.type = `${poke.types[0].type.name}, ${poke.types[1].type.name}`;
+      } else {
+        pokemon.type = poke.types[0].type.name;
+      }
+
+      arrayPoke.push(pokemon);
+    }
+
+    let print = () => {
+      for (let i = 0; i < arrayPoke.length; i++) {
+        let print = document.querySelector(".pokeSubContainer");
+        print.innerHTML += `<div class='item' id="div${arrayPoke[i].id}"\
+                 '><img class='photo pokeImg' src="${arrayPoke[i].image}"><h5 class='card-title pokeItemTitle'>"${arrayPoke[i].name}"\
+                 </h5><p class='pokeP'>Type: "${arrayPoke[i].type}"\
+                 </p><button class='pokeButton add${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='addToFavs(this)'>Add to favs</button><button\
+                  class='remove rem${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='removeToFavs(this)'>Remove</button></div>`;
+      }
     };
-
-    if (poke.types.length > 1) {
-      pokemon.type = `${poke.types[0].type.name}, ${poke.types[1].type.name}`;
-    } else {
-      pokemon.type = poke.types[0].type.name;
-    }
-
-    arrayPoke.push(pokemon);
+    print();
   }
-
-  let print = () => {
-    for (let i = 0; i < arrayPoke.length; i++) {
-      let print = document.querySelector(".pokeSubContainer");
-      print.innerHTML += `<div class='item' id="div${arrayPoke[i].id}"\
-               '><img class='photo pokeImg' src="${arrayPoke[i].image}"><h5 class='card-title pokeItemTitle'>"${arrayPoke[i].name}"\
-               </h5><p class='pokeP'>Type: "${arrayPoke[i].type}"\
-               </p><button class='pokeButton add${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='addToFavs(this)'>Add to favs</button><button\
-                class='remove rem${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='removeToFavs(this)'>Remove</button></div>`;
-    }
-  };
-
-  print();
 
   let search = () => {
     for (let i = 0; i < arrayPoke.length; i++) {
