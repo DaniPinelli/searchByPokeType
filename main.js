5ee4c23e5bdb6bd14bba9a1be3ae3c6915895acc
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-06-28 14:26:35 -0300

    Trying to fix little bug

--- main.js
+++ main.js
@@ -35,67 +35,70 @@ let removeToFavs = (remBtn) => {
 
 let showPoke = async () => {
   let initialArray = JSON.parse(localStorage.getItem("myFavs"));
-  console.log(initialArray.length);
-  if (initialArray.length === 0) {
-    for (let i = 1; i < 13; i++) {
-      let pokesData = await fetch("https://pokeapi.co/api/v2/pokemon/" + i);
-      const poke = await pokesData.json();
-
-      let pokemon = {
-        name: poke.name,
-        id: poke.id,
-        image: poke.sprites.front_default,
-        type: poke.types[0].type.name,
-      };
-
-      if (poke.types.length > 1) {
-        pokemon.type = `${poke.types[0].type.name}, ${poke.types[1].type.name}`;
-      } else {
-        pokemon.type = poke.types[0].type.name;
-      }
-
-      arrayPoke.push(pokemon);
+  if (initialArray.length !== 0) {
+    for (let i = 0; i < initialArray.length; i++) {
+      console.log(initialArray[i].id);
     }
-
-    let print = () => {
-      for (let i = 0; i < arrayPoke.length; i++) {
-        let print = document.querySelector(".pokeSubContainer");
-        print.innerHTML += `<div class='item' id="div${arrayPoke[i].id}"\
-                 '><img class='photo pokeImg' src="${arrayPoke[i].image}"><h5 class='card-title pokeItemTitle'>"${arrayPoke[i].name}"\
-                 </h5><p class='pokeP'>Type: "${arrayPoke[i].type}"\
-                 </p><button class='pokeButton add${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='addToFavs(this)'>Add to favs</button><button\
-                  class='remove rem${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='removeToFavs(this)'>Remove</button></div>`;
-      }
-    };
-    print();
   }
 
-  let search = () => {
+  for (let i = 1; i < 13; i++) {
+    let pokesData = await fetch("https://pokeapi.co/api/v2/pokemon/" + i);
+    const poke = await pokesData.json();
+
+    let pokemon = {
+      name: poke.name,
+      id: poke.id,
+      image: poke.sprites.front_default,
+      type: poke.types[0].type.name,
+    };
+
+    if (poke.types.length > 1) {
+      pokemon.type = `${poke.types[0].type.name}, ${poke.types[1].type.name}`;
+    } else {
+      pokemon.type = poke.types[0].type.name;
+    }
+
+    arrayPoke.push(pokemon);
+  }
+
+  let print = () => {
+    for (let i = 0; i < arrayPoke.length; i++) {
+      let print = document.querySelector(".pokeSubContainer");
+      print.innerHTML += `<div class='item' id="div${arrayPoke[i].id}"\
+                     '><img class='photo pokeImg' src="${arrayPoke[i].image}"><h5 class='card-title pokeItemTitle'>"${arrayPoke[i].name}"\
+                     </h5><p class='pokeP'>Type: "${arrayPoke[i].type}"\
+                     </p><button class='pokeButton add${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='addToFavs(this)'>Add to favs</button><button\
+                      class='remove rem${arrayPoke[i].id}' id='${arrayPoke[i].id}' onclick='removeToFavs(this)'>Remove</button></div>`;
+    }
+  };
+  print();
+};
+
+let search = () => {
+  for (let i = 0; i < arrayPoke.length; i++) {
+    let card = document.getElementById("div" + (i + 1));
+    card.style.display = "none";
+    let searchField = document.getElementById("input");
+    let typeSearched = searchField.value.toLowerCase().trim();
+    if (arrayPoke[i].name.includes(typeSearched)) {
+      card.style.display = "block";
+    }
+  }
+  searchBtn.removeEventListener("click", search);
+};
+
+let searchBtn = document.getElementById("button");
+searchBtn.addEventListener("click", search);
+
+window.addEventListener("keyup", () => {
+  if (document.getElementById("input").value == "") {
+    searchBtn = document.getElementById("button");
+    searchBtn.addEventListener("click", search);
     for (let i = 0; i < arrayPoke.length; i++) {
       let card = document.getElementById("div" + (i + 1));
-      card.style.display = "none";
-      let searchField = document.getElementById("input");
-      let typeSearched = searchField.value.toLowerCase().trim();
-      if (arrayPoke[i].name.includes(typeSearched)) {
-        card.style.display = "block";
-      }
+      card.style.display = "block";
     }
-    searchBtn.removeEventListener("click", search);
-  };
-
-  let searchBtn = document.getElementById("button");
-  searchBtn.addEventListener("click", search);
-
-  window.addEventListener("keyup", () => {
-    if (document.getElementById("input").value == "") {
-      searchBtn = document.getElementById("button");
-      searchBtn.addEventListener("click", search);
-      for (let i = 0; i < arrayPoke.length; i++) {
-        let card = document.getElementById("div" + (i + 1));
-        card.style.display = "block";
-      }
-    }
-  });
-};
+  }
+});
 
 showPoke();
